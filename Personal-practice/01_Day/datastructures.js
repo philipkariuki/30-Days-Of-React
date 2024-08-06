6524034f21a637b6e4e81dd3690f713938a1d512
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-06 23:27:28 +0300

    example on creating an empty array

---
+++ Personal-practice/01_Day/datastructures.js
@@ -0,0 +1,14 @@
+
+// An array is a list of data values in a square bracket
+// Array values are referenced by their index. In JavaScript array index starts at zero.
+// It is very common to use const instead of let to declare an array variable. If you are using const it means you do not use that variable name again.
+
+// creating an empty array using array constructor
+const mtu = Array()
+// or
+let mtu = new Array()
+console.log(mtu)  // []
+
+// creating an empty array using square brackets
+const mtu = []
+console.log(mtu)  // []
