700157d3221912ba49b83c97854735b294b74f3c
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-09 12:06:38 +0300

    solutions for questions 1-3

--- solutions/day-01/exerciseLevel1.js
+++ solutions/day-01/exerciseLevel1.js
@@ -2,3 +2,19 @@
 
 let arey = []
 console.log(arey)
+
+// 2. Declare an array with more than 5 number of elements
+let arey = [
+    'word',
+    true,
+    3,
+    4,
+    5,
+    {firstName:'James', secondName: 'Paul'},
+    {properties: ['car','bike']},
+]
+
+// 3. Find the length of your array
+let areyLength = arey.length 
+console.log(areyLength) // 7
+
