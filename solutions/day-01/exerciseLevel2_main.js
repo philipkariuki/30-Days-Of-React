3261145453b444198ba945a745297ff816b39b76
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-14 03:43:21 +0300

    setting up files for day 1 exercise level 2

---
+++ solutions/day-01/exerciseLevel2_main.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Multiple External Scripts</title>
+    </head>
+        <body>
+            <script src="./countries.js"></script>
+            <script src="./introduction.js"></script>
+        </body>
+</html>
