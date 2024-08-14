3e74020c6138e0913b62c4c6c3c27abe79350e21
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-14 04:46:55 +0300

    solution for exercise level 2 Q1

---
+++ solutions/day-01/exerciseLevel2.js
@@ -0,0 +1,9 @@
+let sentensiString = 'Hii ni mfano ya sentensi. Kiswahili kinatumika nchini Kenya, Tanzania, Uganda'
+console.log(sentensiString)  // Hii ni mfano ya sentensi. Kiswahili kinatumika nchini Kenya, Tanzania, Uganda
+console.log(sentensiString.length) // 77
+
+// First remove all the punctuations and change the string to array and count the number of words in the array
+let sentensiArray = ['Hii', 'ni', 'mfano', 'ya', 'sentensi', 'Kiswahili', 'kinatumika', 'nchini', 'Kenya', 'Tanzania', 'Uganda']
+console.log(sentensiArray)  // [ "Hii", "ni", "mfano", "ya", "sentensi", "Kiswahili", "kinatumika", "nchini", "Kenya", "Tanzania", "Uganda"]
+console.log(sentensiArray.length)  //  11
+
