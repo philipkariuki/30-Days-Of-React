2d46c43c3c1450727345bbd0612b6723690ead79
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-14 05:28:12 +0300

    solution for exercise level 2 Q2

--- solutions/day-01/exerciseLevel2.js
+++ solutions/day-01/exerciseLevel2.js
@@ -7,3 +7,10 @@ let sentensiArray = ['Hii', 'ni', 'mfano', 'ya', 'sentensi', 'Kiswahili', 'kinat
 console.log(sentensiArray)  // [ "Hii", "ni", "mfano", "ya", "sentensi", "Kiswahili", "kinatumika", "nchini", "Kenya", "Tanzania", "Uganda"]
 console.log(sentensiArray.length)  //  11
 
+
+// In the following shopping cart add, remove, edit items
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+
+// Add 'Meat' in the beginning of your shopping cart if it has not been already added
+shoppingCart.splice(0,0, 'Meat') // use splice to add items in this case,first 0 is index position 0, second 0 means number of times to delete, 'Meat' is the item being inserted at index 0
+console.log(shoppingCart)  // [ "Meat", "Milk", "Coffee", "Tea", "Honey" ]
