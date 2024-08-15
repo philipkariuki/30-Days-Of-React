7923a2fda856bac428704d6e5aa3003679d31e1c
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-15 17:20:24 +0300

    solution for exercise level 2 countries.js

--- solutions/day-01/countries.js
+++ solutions/day-01/countries.js
@@ -11,3 +11,11 @@ const countries = [
     'Japan',
     'Kenya',
   ]
+
+// Q1 In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+if (countries.includes('Ethiopia')) {
+    console.log('ETHIOPIA');    //  ETHIOPIA
+} else {
+    countries.push('Ethiopia');
+}
+
