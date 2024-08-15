699095d529500bf20595fe989df3f22e3029d201
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-15 17:11:53 +0300

    solutions for exercise level 2 Q2

--- solutions/day-01/exerciseLevel2.js
+++ solutions/day-01/exerciseLevel2.js
@@ -16,5 +16,38 @@ shoppingCart.splice(0,0, 'Meat') // use splice to add items in this case,first 0
 console.log(shoppingCart)  // [ "Meat", "Milk", "Coffee", "Tea", "Honey" ]
  
 // or(easier method)
-shoppingCart.unshift('Meat')  // inserts new element at start of array
+shoppingCart.unshift('Meat')  // unshift inserts new element at start of array
 console.log(shoppingCart)  //  [ "Meat", "Milk", "Coffee", "Tea", "Honey" ]
+
+
+// add Sugar at the end of you shopping cart if it has not been already added
+shoppingCart.splice(shoppingCart.length,0,'Sugar')
+console.log(shoppingCart)  // [ "Meat", "Milk", "Coffee", "Tea", "Sugar", "Honey", "Sugar" ]
+// or(easier method)
+shoppingCart.push('Sugar');  // .push method adds new items to the end of an array
+console.log(shoppingCart);  // [ "Meat", "Milk", "Coffee", "Tea", "Sugar", "Honey", "Sugar" ]
+
+
+// remove 'Honey' if you are allergic to honey
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+const indx = shoppingCart.indexOf('Honey');   // shoppingCart.indexOf('Honey') returns the index of the first occurrence of ‘Honey’ in the array. If ‘Honey’ is not found, it returns -1.
+
+if (indx !== -1) {   // checks if the index is not equal to -1. This means ‘Honey’ is present in the array
+  shoppingCart.splice(indx, 1);
+}
+console.log(shoppingCart); // ['Milk', 'Coffee', 'Tea', 'Sugar']
+
+
+// modify Tea to 'Green Tea'
+const index = shoppingCart.indexOf('Tea');
+
+if (index !== -1) {
+  shoppingCart[index] = 'Green Tea';
+}
+
+console.log(shoppingCart);  // [ "Milk", "Coffee", "Green Tea", "Honey" ]
+
+
+
+
+
