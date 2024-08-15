d4d98e239166ed835e72659a634d055a88425c42
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-15 17:32:00 +0300

    solution for exercise level 2 webtechs.js

--- solutions/day-01/web_techs.js
+++ solutions/day-01/web_techs.js
@@ -16,4 +16,12 @@ if (webTechs.includes('Sass')) {
     webTechs.push('Sass');
     console.log(webTechs);  // [ "HTML", "CSS", "JavaScript", "React", "Redux", "Node", "MongoDB", "Sass" ]
   }
-  
+
+
+//   Q2 Concatenate the following two variables and store it in a fullStack variable.
+// const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+// const backEnd = ['Node', 'Express', 'MongoDB']
+// console.log(fullStack)
+
+const fullStack = frontEnd.concat(backEnd)
+console.log(fullStack)  //  [ "HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB" ]
