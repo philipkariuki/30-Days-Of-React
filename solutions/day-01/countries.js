e188ef1c412d4fdb6da8b015c73a569647d8993e
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-19 04:45:46 +0300

    solution for exercise level 3 Q4

--- solutions/day-01/countries.js
+++ solutions/day-01/countries.js
@@ -31,3 +31,15 @@ let middleCtry = countries[middleCtryIndex]  // value of index 5
 console.log(middleCtry)  // Finland
 
 
+// Q4 Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
+
+
+
+const middleIndex = Math.ceil(countries.length / 2);  // we use math.ceil method to round up so that the first half has more elements if not even i.e. odd
+
+// slice selects from a given start, up to but not inclusive of a given end
+const firstHalf = countries.slice(0, middleIndex);  // selects elements from index 0 to 6 but doesn't include element 6
+const secondHalf = countries.slice(middleIndex); // selects elements from index 6 to the end by default, unless the intended end is defined
+
+console.log(firstHalf); // [ "Albania", "Bolivia", "Canada", "Denmark", "Ethiopia", "Finland" ]
+console.log(secondHalf); // [ "Germany", "Hungary", "Ireland", "Japan", "Kenya" ]
