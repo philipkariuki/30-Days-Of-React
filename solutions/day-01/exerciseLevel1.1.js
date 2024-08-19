b765573880ab99fcbb16824b828ddcbd6c4c610d
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-19 18:54:07 +0300

    solution for exerciseLevel1.1 Q3.1

--- solutions/day-01/exerciseLevel1.1.js
+++ solutions/day-01/exerciseLevel1.1.js
@@ -26,3 +26,17 @@ if (yourAge > myAge) {
 } else {
     console.log("We are the same age.");
 }
+
+
+// Q3 If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in two ways using: i)if else
+//                                                                                                                       ii)ternary operator
+// i)Using if else
+let a = 4
+let b = 3
+
+if (a > b) {
+    console.log(`${a} is greater than ${b}`);  // 4 is greater than 3
+} else if (a < b) {
+    console.log(`${a} is less than ${b}`);
+}
+
