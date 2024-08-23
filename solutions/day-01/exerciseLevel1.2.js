d8806dd56edd11d7af3a33c0c2ab1f22d5597674
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-23 06:08:34 +0300

    solution for exerciseLevel1.2 Q3

--- solutions/day-01/exerciseLevel1.2.js
+++ solutions/day-01/exerciseLevel1.2.js
@@ -3,3 +3,17 @@ let dog = {};
 
 // Q2 Print the the dog object on the console
 console.log(dog)  // Object {  }
+
+// Q3 Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+let dog = {
+    name: "Rosco",
+    legs: 4,
+    color: "brown",
+    age: 2,
+    bark: function() {
+        return "woof woof";
+    }
+};
+
+console.log(dog)  //  Object { name: "Rosco", legs: 4, color: "brown", age: 2, bark: bark() }
+console.log(dog.bark()) // woof woof
