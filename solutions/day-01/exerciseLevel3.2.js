869e209af90779eb76ae4779893e03c8ee4e73ad
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-08-30 15:44:54 +0300

    solution for exerciseLevel3.2 Q2

--- solutions/day-01/exerciseLevel3.2.js
+++ solutions/day-01/exerciseLevel3.2.js
@@ -62,3 +62,126 @@ console.log('Account Balance:', personAccount.accountBalance());
 // Account Balance: 7050
 
 
+// Questions:2, 3 and 4 are based on the following two arrays:users and products ()
+
+const users = [
+    {
+      _id: 'ab12ex',
+      username: 'Alex',
+      email: 'alex@alex.com',
+      password: '123123',
+      createdAt: '08/01/2020 9:00 AM',
+      isLoggedIn: false,
+    },
+    {
+      _id: 'fg12cy',
+      username: 'Asab',
+      email: 'asab@asab.com',
+      password: '123456',
+      createdAt: '08/01/2020 9:30 AM',
+      isLoggedIn: true,
+    },
+    {
+      _id: 'zwf8md',
+      username: 'Brook',
+      email: 'brook@brook.com',
+      password: '123111',
+      createdAt: '08/01/2020 9:45 AM',
+      isLoggedIn: true,
+    },
+    {
+      _id: 'eefamr',
+      username: 'Martha',
+      email: 'martha@martha.com',
+      password: '123222',
+      createdAt: '08/01/2020 9:50 AM',
+      isLoggedIn: false,
+    },
+    {
+      _id: 'ghderc',
+      username: 'Thomas',
+      email: 'thomas@thomas.com',
+      password: '123333',
+      createdAt: '08/01/2020 10:00 AM',
+      isLoggedIn: false,
+    },
+]
+
+
+const products = [
+    {
+      _id: 'eedfcf',
+      name: 'mobile phone',
+      description: 'Huawei Honor',
+      price: 200,
+      ratings: [
+        { userId: 'fg12cy', rate: 5 },
+        { userId: 'zwf8md', rate: 4.5 },
+      ],
+      likes: [],
+    },
+    {
+      _id: 'aegfal',
+      name: 'Laptop',
+      description: 'MacPro: System Darwin',
+      price: 2500,
+      ratings: [],
+      likes: ['fg12cy'],
+    },
+    {
+      _id: 'hedfcg',
+      name: 'TV',
+      description: 'Smart TV:Procaster',
+      price: 400,
+      ratings: [{ userId: 'fg12cy', rate: 5 }],
+      likes: ['fg12cy'],
+    },
+]
+
+// Q2 Imagine you are getting the above users collection from a MongoDB database. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account
+
+function signUp(newUser) {
+    // Check if the user already exists
+    const userExists = users.some(user => user.email === newUser.email);  // .some() is an array method that checks if at least one element in the array passes the test implemented by the provided function.
+    // Here, the arrow function is used as the test function for .some(). It checks if the email property of any user in the users array matches the email property of newUser.
+  
+    if (userExists) {
+      return 'User already has an account';
+    } else {
+      // Add new user to the users collection
+      users.push(newUser);
+      return 'User successfully signed up';
+    }
+  }
+
+ /*  alternative
+  function signUp(newUser) {
+    let userExists = false;                       // Here we initialize userExists to false
+    for (let i = 0; i < users.length; i++) {     // We loop through each user in the users array
+      if (users[i].email === newUser.email) {   // If we find a user with the same email as newUser, we set userExists to true and break out of the loop
+        userExists = true;
+        break;
+      }
+    }
+    
+    if (userExists) {
+      return 'User already has an account';
+    } else {      
+      users.push(newUser);
+      return 'User successfully signed up';
+    }
+    } */
+  
+  // Example:
+  const newUser = {
+    _id: 'newId',
+    username: 'NewUser',
+    email: 'newuser@newuser.com',
+    password: 'newpassword',
+    createdAt: new Date().toLocaleString(),
+    isLoggedIn: false,
+  };
+  
+  console.log(signUp(newUser)); // User successfully signed up
+  console.log(signUp(newUser)); // User already has an account
+  
