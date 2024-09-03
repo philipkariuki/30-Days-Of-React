d0b2b3d52d7f19961b2091e005b95ea6692de32a
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-09-03 18:05:37 +0300

    solution for exerciseLevel1.4 Q2

--- solutions/day-01/exercideLevel1.4.js
+++ solutions/day-01/exercideLevel1.4.js
@@ -8,4 +8,17 @@ function fullName(firstName, lastName) {
 console.log(fullName("Jina", "Lako")); // Jina Lako
 
 
+// Q2 Declare a function addNumbers and it takes two two parameters and it returns the sum
+function addNumbers(a, b) {
+    return a + b;
+}
+
+console.log(addNumbers(5, 3)); // 8
+
+// shorter method using arrow functions
+let addNumberz = (e,f) => e + f
+
+console.log(addNumbers(3,4)); // 7
+
+
 
