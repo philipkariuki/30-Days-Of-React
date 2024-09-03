659ed33542091b50efa2eee759811bab5d754357
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-09-03 17:54:47 +0300

    Creates exeriseLevel1.4.js
    solution for exerciseLevel1.4 Q1

---
+++ solutions/day-01/exercideLevel1.4.js
@@ -0,0 +1,11 @@
+// Q1     Declare a function fullName and it takes firstName, lastName as a parameter and it returns your full name
+
+function fullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+
+// Example usage:
+console.log(fullName("Jina", "Lako")); // Jina Lako
+
+
+
