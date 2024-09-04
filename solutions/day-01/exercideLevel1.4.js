c9446df05a1e927a35e90eeacbb4f4fa3cf539c0
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-09-05 00:24:55 +0300

    solution for exerciseLevel1.4 Q5

--- solutions/day-01/exercideLevel1.4.js
+++ solutions/day-01/exercideLevel1.4.js
@@ -54,3 +54,44 @@ const convertCelciusToFahrenheits = celsius => Math.round(celsius * 9/5) + 32
 console.log(convertCelciusToFahrenheit(37.8))   // 100
 
 
+// Q5     Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
+    // The same groups apply to both men and women.
+    // Underweight: BMI is less than 18.5
+    // Normal weight: BMI is 18.5 to 24.9
+    // Overweight: BMI is 25 to 29.9
+    // Obese: BMI is 30 or more
+
+/
+// i) Write a function which calculates bmi
+function bmi(weight,height){
+    return weight/(height*height)
+  }
+console.log(bmi(72,1.76))  // 23.243801652892564
+
+// ii) Function to determine bmi category
+function checkBmi(w,h){
+    let fullBmi = w/(h*h)
+    let bmi = fullBmi.toFixed(2)  // truncates to 2 decimal places
+    const underweight = bmi < 18.5
+    const normalweight = bmi >= 18.5 && bmi <= 24.9
+    const overweight = bmi >= 25 && bmi <= 29.9
+    const obese = bmi >= 30
+
+    if (underweight) {
+        console.log(`Your bmi of ${bmi} is underweight ` )
+    }
+    else if (normalweight) {
+        console.log(`Your bmi of ${bmi} is normal weight ` )
+    }
+    else if (overweight) {
+        return { bmi, category: 'overweight' }
+    }
+    else if (obese) {
+        console.log(`Your bmi of ${bmi} is obese ` )
+    }
+  }
+
+console.log(checkBmi(70,1.77))  // Your bmi of 22.34 is normal weight
+console.log(checkBmi(85,1.8))  //  { bmi: "26.23", category: "overweight" }
+
+
