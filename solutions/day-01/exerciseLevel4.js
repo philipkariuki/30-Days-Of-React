f0c0f6055225d8052e2bf0fb58aac59b02f35640
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-09-11 01:12:32 +0300

    solution for exerciseLevel4 Q1 i)

--- solutions/day-01/exerciseLevel4.js
+++ solutions/day-01/exerciseLevel4.js
@@ -26,3 +26,20 @@ const person = {
 
 // Q Create a function called getPersonInfo. The getPersonInfo function takes an object parameter. The structure of the object and the output of the function is given as shown above. Try to use both a regular way and destructuring and compare the cleanness of the code.
 
+// i) Regular way
+function getPersonInfo(person) {
+    const firstName = person.firstName;
+    const lastName = person.lastName;
+    const age = person.age;
+    const country = person.country;
+    const job = person.job;
+    const skills = person.skills.join(', ');
+    const languages = person.languages.join(', ');
+  
+    return `${firstName} ${lastName} lives in ${country}. He is ${age} years old. He is an ${job}. He teaches ${skills}. He speaks ${languages}.`;
+  }
+    
+console.log(getPersonInfo(person));  // Asabeneh Yetayeh lives in Finland. He is 250 years old. He is an Instructor and Developer. He teaches HTML, CSS, JavaScript, React, Redux, Node, MongoDB, Python, D3.js. He speaks Amharic, English, Suomi(Finnish).
+
+
+
