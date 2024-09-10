06592302b8e8f096bce80dc80bbdd3a5501fb279
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-09-11 01:05:46 +0300

    Creates exerciseLevel4.js

---
+++ solutions/day-01/exerciseLevel4.js
@@ -0,0 +1,28 @@
+const person = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    job: 'Instructor and Developer',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Redux',
+      'Node',
+      'MongoDB',
+      'Python',
+      'D3.js',
+    ],
+    languages: ['Amharic', 'English', 'Suomi(Finnish)'],
+  }
+
+
+
+// output
+// Asabeneh Yetayeh lives in Finland. He is  250 years old. He is an Instructor and Developer. He teaches HTML, CSS, JavaScript, React, Redux, Node, MongoDB, Python and D3.js. He speaks Amharic, English and a little bit of Suomi(Finnish)
+
+
+// Q Create a function called getPersonInfo. The getPersonInfo function takes an object parameter. The structure of the object and the output of the function is given as shown above. Try to use both a regular way and destructuring and compare the cleanness of the code.
+
