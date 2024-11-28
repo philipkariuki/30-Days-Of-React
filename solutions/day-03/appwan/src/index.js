8a5fb36ed0838450f5bea1544b7dc4e968156699
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-11-29 01:57:37 +0300

    We update index.js in accordance with the tutorial.The changes are reflected in our react app on the browser
    In the comments we include an updated method of achieving the same result since the tutorial uses an older version of React

--- solutions/day-03/appwan/src/index.js
+++ solutions/day-03/appwan/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//index.js
+// importing the react and react-dom package
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const jsxElement = <h1>This is a JSX element</h1>
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(jsxElement, rootElement)
+
+
+// Using updated method which is supposed to improve performance
+/* import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const jsxElement = <h1>This is a JSX element</h1>;
+const rootElement = document.getElementById('root');
+
+// Use createRoot to initialize the root
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {jsxElement}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ */
