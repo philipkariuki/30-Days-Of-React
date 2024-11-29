433456d73140493c739d8b2b244dd01ad63d321e
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-11-29 17:58:11 +0300

    Edits index.js by adding more jsx elements; header,main,footer and app

--- solutions/day-03/appwan/src/index.js
+++ solutions/day-03/appwan/src/index.js
@@ -4,6 +4,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// JSX element, header
 const header = (
   <header>
     <h1>Welcome to 30 Days Of React</h1>
@@ -13,9 +14,39 @@ const header = (
     <small>Nov 29, 2024</small>
   </header>
 )
+
+// JSX element, main
+const main = (
+  <main>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+
+// JSX element, footer
+const footer = (
+  <footer>
+    <p>Copyright 2024</p>
+  </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(header, rootElement)
+ReactDOM.render(app, rootElement)
 
 
 // Using updated method which is supposed to improve performance
@@ -31,13 +62,42 @@ const header = (
     <small>Nov 29, 2024</small>
   </header>
 )
+
+// JSX element, main
+const main = (
+  <main>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+
+// JSX element, footer
+const footer = (
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
 const rootElement = document.getElementById('root');
 
 // Use createRoot to initialize the root
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    {header}
+    {app}
   </React.StrictMode>
 );
  */
