aac5cce9c632901e616349d6a82389e500747699
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-11-29 17:51:02 +0300

    Edits index.js by adding more jsx elements

--- solutions/day-03/appwan/src/index.js
+++ solutions/day-03/appwan/src/index.js
@@ -4,24 +4,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const jsxElement = <h1>This is a JSX element</h1>
+const header = (
+  <header>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Mzee Msee</p>
+    <small>Nov 29, 2024</small>
+  </header>
+)
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(jsxElement, rootElement)
+ReactDOM.render(header, rootElement)
 
 
 // Using updated method which is supposed to improve performance
 /* import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const jsxElement = <h1>This is a JSX element</h1>;
+const header = (
+  <header>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Mzee Msee</p>
+    <small>Nov 29, 2024</small>
+  </header>
+)
 const rootElement = document.getElementById('root');
 
 // Use createRoot to initialize the root
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    {jsxElement}
+    {header}
   </React.StrictMode>
 );
  */
