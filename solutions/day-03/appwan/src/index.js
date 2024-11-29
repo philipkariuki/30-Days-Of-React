fcd60f5a1a57c4f81f97d41ab49af10074d22d57
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-11-29 18:28:45 +0300

    Injecting data to the jsx elements

--- solutions/day-03/appwan/src/index.js
+++ solutions/day-03/appwan/src/index.js
@@ -6,37 +6,63 @@ import ReactDOM from 'react-dom'
 
 
 // JSX element, header
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Mzee',
+  lastName: 'Msee',
+}
+const date = 'Nov 29, 2024'
+
+
 const header = (
   <header>
     <div className='header-wrapper'>
-      <h1>Welcome to 30 Days Of React</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
-      <p>Mzee Msee</p>
-      <small>Nov 29, 2024</small>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
     </div>    
   </header>
 )
 
+const yearBorn = 1904
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
 // JSX element, main
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+
 const main = (
   <main>
     <div className='main-wrapper'>
       <p>Prerequisite to get started react.js:</p>
       <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
+        {techsFormatted}
       </ul>
+      {personAge}
     </div>    
   </main>
 )
 
 // JSX element, footer
+const copyRight = 'Copyright 2020'
+
 const footer = (
   <footer>
     <div className='footer-wrapper'>
-      <p>Copyright 2024</p>
+      <p>{copyRight}</p>
     </div>
     
   </footer>
