ebad8fbc4592f407a9eb45fabc357a47d16a2ea6
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-12-06 04:04:41 +0300

    Solutions for exercise level 2
    First we import and render images
    
    Second we use html elements to create a subscribe form

--- solutions/day-03/appwan/src/index - Copy.js
+++ solutions/day-03/appwan/src/index - Copy.js
@@ -3,80 +3,79 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import cssPicha from './images/css.jpg'
-import htmlPicha from './images/html.jpg'
-import jsPicha from './images/js.jpg'
+import dogoPicha from './images/dogo.jpg'
+import { changeHeaderColor } from './util' // Import the changeHeaderColor 
+
 
 
 // JSX element, header
-const title = 'Front End Technologies'
-
-const css = (
-  <div>
-    <img src={cssPicha} alt='css' />
-  </div>
-)
-
-const html = (
-  <div>
-    <img src={htmlPicha} alt='html' />
-  </div>
-)
-
-const js = (
-  <div>
-    <img src={jsPicha} alt='js' />
-  </div>
-)
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Mzee',
+  lastName: 'Msee',
+}
+const date = 'Nov 29, 2024'
 
 
 const header = (
   <header>
     <div className='header-wrapper'>
-      <div className='title-container'>
-        <h4>{title}</h4>
-      </div>
-      {html}
-      {css}
-      {js}
-      {html}
-    </div>   
+      <h1 id='welcome-title'>{welcome}</h1> {/* Add id to the element */}
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
+    </div>    
   </header>
 )
 
+const yearBorn = 1904
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
 // JSX element, main
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
 
+const user = (
+  <div>
+    <img src={dogoPicha} alt='dogo' />
+  </div>
+)
 
+const handleChangeColor = () => { changeHeaderColor('welcome-title')  
+}
 
 const main = (
   <main>
     <div className='main-wrapper'>
-      
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        {techsFormatted}
+      </ul>
+      {personAge}
+      {user}
+      <button onClick={handleChangeColor}>Change Header Color</button> {/* Add the button */} 
     </div>    
   </main>
 )
 
 // JSX element, footer
-const subscribe = 'Subscribe'
-const message = 'Sign up with your email address to receive news and updates'
-
-const SignupForm = () => (
-  <form className='signup-form'>
-    <input type='text' placeholder='First Name' name='firstname' required/>
-    <input type='text' placeholder='Last Name' name='lastname' required />
-    <input type='email' placeholder='Email' name='email' required />
-  </form>
-)
+const copyRight = 'Copyright 2024'
 
 const footer = (
   <footer>
     <div className='footer-wrapper'>
-      <div className='title-container'>
-        <h1 id='supscribe'>{subscribe}</h1>
-        <p>{message}</p>
-        <SignupForm />
-        <button type='submit'>Subscribe</button>
-      </div>    
+      <p>{copyRight}</p>
     </div>
     
   </footer>
