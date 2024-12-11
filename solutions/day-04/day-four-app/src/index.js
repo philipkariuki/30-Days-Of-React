925e74643d99e6633ca5162c28b8f1b9f611d7e7
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-12-11 20:08:47 +0300

    Initial setup for day-04. We setup and configure files such as index.html and index.js to match the project's requirements and we delete all unnecessary files

---
+++ solutions/day-04/day-four-app/src/index.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import logoNdogo from './images/logors.png'
+
+
+
+// JSX element, header
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Mzee',
+  lastName: 'Msee',
+}
+const date = 'Nov 29, 2024'
+
+
+const header = (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
+    </div>    
+  </header>
+)
+
+const yearBorn = 1904
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
+// JSX element, main
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+const ogo = (
+  <div>
+    <img src={logoNdogo} alt='dogo' />
+  </div>
+)
+
+
+const main = (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        {techsFormatted}
+      </ul>
+      {personAge}
+      {ogo}
+    </div>    
+  </main>
+)
+
+// JSX element, footer
+const copyRight = 'Copyright 2024'
+
+const footer = (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>{copyRight}</p>
+    </div>
+    
+  </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+  <div className='app'>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+
+const rootElement = document.getElementById('root');
+
+// Use createRoot to initialize the root
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    {app}
+  </React.StrictMode>
+);
+
+
