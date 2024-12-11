c880674ca01c31cc087bbb832aa26b362d232390
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-12-12 02:34:06 +0300

    We update index.js with more components namely User card,Techlist,Main and Footer components. All these components are wrapped together inside the App component which is a parent or container component

--- solutions/day-04/day-four-app/src/index.js
+++ solutions/day-04/day-four-app/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import logoNdogo from './images/logors.png'
+import logoNdogo from './images/logors.png'
 
 
 
 // JSX element, header
-const welcome = 'Welcome to 30 Days Of React'
-const title = 'Getting Started React'
-const subtitle = 'JavaScript Library'
-const author = {
-  firstName: 'Mzee',
-  lastName: 'Msee',
-}
-const date = 'Nov 29, 2024'
+// const welcome = 'Welcome to 30 Days Of React'
+// const title = 'Getting Started React'
+// const subtitle = 'JavaScript Library'
+// const author = {
+//   firstName: 'Mzee',
+//   lastName: 'Msee',
+// }
+// const date = 'Nov 29, 2024'
 
 
 // const header = (
@@ -34,34 +34,96 @@ const date = 'Nov 29, 2024'
 //   return header
 // }
 
-// Explicitly returning the JSX
-const Header = () => (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
-    </div> 
-  </header>
+// Header component
+// const Header = () => (
+//   <header>
+//     <div className='header-wrapper'>
+//       <h1>{welcome}</h1>
+//       <h2>{title}</h2>
+//       <h3>{subtitle}</h3>
+//       <p>
+//         Instructor: {author.firstName} {author.lastName}
+//       </p>
+//       <small>Date: {date}</small>
+//     </div> 
+//   </header>
+// )
+
+// Header component
+const Header = () => {
+  return (
+    <header>
+      <div className='header-wrapper'>
+        <h1>Welcome to 30 Days Of React</h1>
+        <h2>Getting Started React</h2>
+        <h3>JavaScript Library</h3>
+        <p>Mzee Msee</p>
+        <small>Nov 29, 2024</small>
+      </div>
+    </header>
+  )
+}
+
+// User card component
+const UserCard = () => (
+  <div className='user-card'>
+    <img src={logoNdogo} alt='dogo' />
+    <h4>Mister Recat</h4>
+  </div>
 )
 
-// const Header = () => {
-//   return (
-//     <header>
-//       <div className='header-wrapper'>
-//         <h1>Welcome to 30 Days Of React</h1>
-//         <h2>Getting Started React</h2>
-//         <h3>JavaScript Library</h3>
-//         <p>Asabeneh Yetayeh</p>
-//         <small>Oct 3, 2020</small>
-//       </div>
-//     </header>
-//   )
-// }
+
+// TechList Component
+const TechList = () => {
+  const techs = ['HTML', 'CSS', 'JavaScript']
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
+}
+
+
+// Main Component
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList />
+      </ul>
+      <UserCard />
+    </div>
+  </main>
+)
+
+
+// Footer Component
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright 2024</p>
+    </div>
+  </footer>
+)
+
+
+// The app or parent or container component
+const App = () => (
+  <div className='app'>
+    <Header /> {/* Render Header as a component */}
+    <Main /> {/* Render Main as a component */}
+    <Footer /> {/* Render Footer as a component */}
+  </div>
+)
+
+
+const rootElement = document.getElementById('root');
+
+// Use createRoot to initialize the root
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App /> {/* Render the App component */}
+  </React.StrictMode>
+)
 
 
 
@@ -75,15 +137,9 @@ const personAge = (
   </p>
 )
 
-// JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>)
 
-const ogo = (
-  <div>
-    <img src={logoNdogo} alt='dogo' />
-  </div>
-)
+
+
 
 
 const main = (
@@ -111,20 +167,4 @@ const footer = (
   </footer>
 ) */
 
-// JSX element, app, a container or a parent
-const app = (
-  <div className='app'>
-    <Header /> {/* Render Header as a component */}
-  </div>
-)
 
-
-const rootElement = document.getElementById('root');
-
-// Use createRoot to initialize the root
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    {app}
-  </React.StrictMode>
-);
