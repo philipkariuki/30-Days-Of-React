002a11e8636caebbda514d87eb1f01ac9d130967
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2024-12-17 20:05:01 +0300

    Solutions for day 4 exercise level2 and exercise level3
    Took a while to implement styling and combine all the exercises in one screen

---
+++ solutions/day-04/day-four-app/src/index - Copy.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import dogoPicha from './images/dogo.jpg'
+
+
+
+// JSX element, header
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Mzee',
+  lastName: 'Msee',
+}
+const date = 'Nov 29, 2024'
+
+
+// const header = (
+//   <header>
+//     <div className='header-wrapper'>
+//       <h1>{welcome}</h1>
+//       <h2>{title}</h2>
+//       <h3>{subtitle}</h3>
+//       <p>
+//         Instructor: {author.firstName} {author.lastName}
+//       </p>
+//       <small>Date: {date}</small>
+//     </div>    
+//   </header>
+// )
+
+// // React Component
+// const Header = () => {
+//   return header
+// }
+
+// Header component
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
+    </div> 
+  </header>
+)
+
+
+// User card component
+const dog = {
+  firstName: 'Cool',
+  lastName: 'Dog',
+}
+
+const UserCard = () => (
+  <div className='user-card'>
+    <img src={dogoPicha} alt='dogo' />
+    <h4>{dog.firstName} {dog.lastName}</h4>
+  </div>
+)
+
+
+// TechList Component
+const TechList = () => {
+  const techs = ['HTML', 'CSS', 'JavaScript']
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
+}
+
+
+// Main Component
+const yearBorn = 1904
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList />
+      </ul>
+      {personAge}
+      <UserCard />
+    </div>
+  </main>
+)
+
+// Hexadecimal color generator
+const hexaColor = () => {
+  let str = '0123456789abcdef'
+  let color = ''
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length)
+    color += str[index]
+  }
+  return '#' + color
+}
+
+const HexaColor = () => <div>{hexaColor()}</div>
+
+// Footer Component
+const copyRight = 'Copyright 2024'
+
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>{copyRight}</p>
+    </div>
+  </footer>
+)
+
+
+// The app or parent or container component
+const App = () => (
+  <div className='app'>
+    <Header /> {/* Render Header as a component */}
+    <Main /> {/* Render Main as a component */}
+    <HexaColor />
+    <Footer /> {/* Render Footer as a component */}
+  </div>
+)
+
+
+const rootElement = document.getElementById('root');
+
+// Use createRoot to initialize the root
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App /> {/* Render the App component */}
+  </React.StrictMode>
+)
--- solutions/day-04/day-four-app/src/index.js
+++ solutions/day-04/day-four-app/src/index.js
@@ -1,121 +1,140 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import dogoPicha from './images/dogo.jpg'
+import cssPicha from './images/css.jpg'
+import htmlPicha from './images/html.jpg'
+import jsPicha from './images/js.jpg'
+import rctPicha from './images/logors.png'
 
 
 
 // JSX element, header
-const welcome = 'Welcome to 30 Days Of React'
-const title = 'Getting Started React'
-const subtitle = 'JavaScript Library'
-const author = {
-  firstName: 'Mzee',
-  lastName: 'Msee',
-}
-const date = 'Nov 29, 2024'
+const title = 'Front End Technologies'
 
 
-// const header = (
-//   <header>
-//     <div className='header-wrapper'>
-//       <h1>{welcome}</h1>
-//       <h2>{title}</h2>
-//       <h3>{subtitle}</h3>
-//       <p>
-//         Instructor: {author.firstName} {author.lastName}
-//       </p>
-//       <small>Date: {date}</small>
-//     </div>    
-//   </header>
-// )
-
-// // React Component
-// const Header = () => {
-//   return header
-// }
-
-// Header component
-const Header = () => (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
-    </div> 
-  </header>
+const Czz = () => (
+  <div className='czz-card'>
+    <img src={cssPicha} alt='czz' />
+  </div>
 )
 
+const Htmls = () => (
+  <div className='html-card'>
+    <img src={htmlPicha} alt='htmls' />
+  </div>
+)
 
-// User card component
-const dog = {
-  firstName: 'Cool',
-  lastName: 'Dog',
-}
+const Js = () => (
+  <div className='js-card'>
+    <img src={jsPicha} alt='js' />
+  </div>
+)
 
-const UserCard = () => (
-  <div className='user-card'>
-    <img src={dogoPicha} alt='dogo' />
-    <h4>{dog.firstName} {dog.lastName}</h4>
+const Ract = () => (
+  <div className='ract-card'>
+    <img src={rctPicha} alt='react' />
   </div>
 )
 
 
-// TechList Component
-const TechList = () => {
-  const techs = ['HTML', 'CSS', 'JavaScript']
-  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
-  return techsFormatted
-}
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <div className='title-container'>
+        <h4>{title}</h4>
+      </div>
+      <Htmls />
+      <Czz />
+      <Js />
+      <Ract />
+    </div>   
+  </header>
+)
+
+
+
+
+// User card component
+
+const UserCard = () => (
+  <div>
+    <img src={dogoPicha} alt='dogo' />
+  </div>
+)
 
 
 // Main Component
-const yearBorn = 1904
-const currentYear = new Date().getFullYear()
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-)
+
+
 
 const Main = () => (
   <main>
     <div className='main-wrapper'>
-      <p>Prerequisite to get started react.js:</p>
-      <ul>
-        <TechList />
-      </ul>
-      {personAge}
-      <UserCard />
+      <div className='main-container'>
+        <p><UserCard /></p>
+        <p>MZEE MSEE <i className="fas fa-check-circle"></i></p>
+        <small>Front End Developer</small>
+        <p><b>Skills</b></p>
+        <div className='skills-container'>
+          <p>
+            <span>HTML</span><span>CSS</span><span>Sass</span><span>JS</span><span>React</span><span>Redux</span><span>Node</span><span>MongoDB</span><span>Python</span><span>Flask</span><span>Django</span><span>NumPy</span><span>Pandas</span><span>Data Analysis</span><span>MYSQL</span><span>PostgreSQL</span><span>Docker</span><span>Heroku</span><span>Git</span><span>Django</span><span>NumPy</span><span>Pandas</span><span>MYSQL</span><span>PostgreSQL</span><span>Docker</span><span>Heroku</span><span>Git</span><span>Django</span><span>NumPy</span><span>Pandas</span><span>MYSQL</span>
+          </p>
+        </div>
+        <small><i className="fas fa-light fa-clock"></i> Joined on Dec 2, 2024</small>
+      </div>  
     </div>
   </main>
 )
 
-// Hexadecimal color generator
-const hexaColor = () => {
-  let str = '0123456789abcdef'
-  let color = ''
+// Hexadecimal color generator component
+const generateRandomColor = () => {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
   for (let i = 0; i < 6; i++) {
-    let index = Math.floor(Math.random() * str.length)
-    color += str[index]
+    color += letters[Math.floor(Math.random() * 16)]
   }
-  return '#' + color
+  return color
+}
+const ColorRows = () => {
+  const rows = Array.from({ length: 4 }, () => generateRandomColor())
+  return (
+    <div className="color-container">
+      {rows.map((color, index) => (
+        <div
+          key={index}
+          className="color-row"
+          style={{ backgroundColor: color }}
+        >
+          {color}
+        </div>
+      ))}
+    </div>
+  )
 }
 
-const HexaColor = () => <div>{hexaColor()}</div>
-
 // Footer Component
-const copyRight = 'Copyright 2024'
+
+const subscribe = 'Subscribe'
+const message = 'Sign up with your email address to receive news and updates'
+
+const SignupForm = () => (
+  <form className='signup-form'>
+    <input type='text' placeholder='First Name' name='firstname' required/>
+    <input type='text' placeholder='Last Name' name='lastname' required />
+    <input type='email' placeholder='Email' name='email' required />
+  </form>
+)
 
 const Footer = () => (
   <footer>
     <div className='footer-wrapper'>
-      <p>{copyRight}</p>
-    </div>
+      <div className='title-container'>
+        <h1 id='supscribe'>{subscribe}</h1>
+        <p>{message}</p>
+        <SignupForm />
+        <button type='submit'>Subscribe</button>
+      </div>    
+    </div>    
   </footer>
 )
 
@@ -125,7 +144,8 @@ const App = () => (
   <div className='app'>
     <Header /> {/* Render Header as a component */}
     <Main /> {/* Render Main as a component */}
-    <HexaColor />
+    <generateRandomColor />
+    <ColorRows />
     <Footer /> {/* Render Footer as a component */}
   </div>
 )
