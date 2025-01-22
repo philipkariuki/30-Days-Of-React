b843f600b1572d2cc163d1901efb07f6b2e74aa9
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2025-01-22 14:09:33 +0300

    Example of Object props type

--- solutions/day-05/day-five-app/src/index.js
+++ solutions/day-05/day-five-app/src/index.js
@@ -3,20 +3,66 @@ import ReactDOM from 'react-dom/client'
 
 
 
-// d) Array props type:
+// e) Object props type:
 
 
-const Skills = (props) => {
-// modifying the skills array
-const skillList = props.skills.map((skill) => <li>{skill}</li>)
-return <ul>{skillList}</ul>
+const showDate = (time) => {
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+]
+
+const month = months[time.getMonth()].slice(0, 3)
+const year = time.getFullYear()
+const date = time.getDate()
+return ` ${month} ${date}, ${year}`
+}
+// Header Component
+const Header = (props) => {
+return (
+    <header>
+    <div className='header-wrapper'>
+        <h1>{props.data.welcome}</h1>
+        <h2>{props.data.title}</h2>
+        <h3>{props.data.subtitle}</h3>
+        <p>
+        {props.data.author.firstName} {props.data.author.lastName}
+        </p>
+        <small>{showDate(props.data.date)}</small>
+    </div>
+    </header>
+)
 }
 
-const App = () => (
-<div className='app'>
-    <Skills skills={['HTML', 'CSS', 'JavaScript']} />
-</div>
+// The app or parent or container component
+const App = () => {
+const data = {
+    welcome: 'Welcome to 30 Days Of React',
+    title: 'Getting Started React',
+    subtitle: 'JavaScript Library',
+    author: {
+    firstName: 'Mzee',
+    lastName: 'Msee',
+    },
+    date: new Date(), // date needs to be formatted to a human readable format
+}
+
+return (
+    <div className='app'>
+    <Header data={data} />
+    </div>
 )
+}
 
 
 const rootElement = document.getElementById('root');
