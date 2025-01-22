c3558d85c7162ffa3af6bcf9f15a15230fce1e88
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2025-01-22 14:18:23 +0300

    Another example of Function props type

--- solutions/day-05/day-five-app/src/index.js
+++ solutions/day-05/day-five-app/src/index.js
@@ -3,22 +3,47 @@ import ReactDOM from 'react-dom/client'
 
 
 
-// e) Function props type:// The app or parent or container component
+// e) Function props type:
 
 
-// A button component
+// Function to display time in Mon date, year format eg Oct 4, 2020
+const showDate = (time) => {
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+]
+
+const month = months[time.getMonth()].slice(0, 3)
+const year = time.getFullYear()
+const date = time.getDate()
+return ` ${month} ${date}, ${year}`
+}
+
+// Button component
 const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
-// The App, or the parent or the container component
+// The app or parent or container component
 const App = () => {
+const handleTime = () => {
+    alert(showDate(new Date()))
+}
 const greetPeople = () => {
     alert('Welcome to 30 Days Of React Challenge, 2024')
 }
-
 return (
     <div className='app'>
+    <Button text='show time' onClick={handleTime} />
     <Button text='Greet People' onClick={greetPeople} />
-    <Button text='Show Time' onClick={() => alert(new Date())} />
     </div>
 )
 }
