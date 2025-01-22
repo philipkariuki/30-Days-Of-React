5f5f28d382339055cde64bc30f8976954ba14e30
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2025-01-22 14:14:52 +0300

    Example of Function props type

--- solutions/day-05/day-five-app/src/index.js
+++ solutions/day-05/day-five-app/src/index.js
@@ -3,63 +3,22 @@ import ReactDOM from 'react-dom/client'
 
 
 
-// e) Object props type:
+// e) Function props type:// The app or parent or container component
 
 
-const showDate = (time) => {
-const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-]
+// A button component
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
-const month = months[time.getMonth()].slice(0, 3)
-const year = time.getFullYear()
-const date = time.getDate()
-return ` ${month} ${date}, ${year}`
-}
-// Header Component
-const Header = (props) => {
-return (
-    <header>
-    <div className='header-wrapper'>
-        <h1>{props.data.welcome}</h1>
-        <h2>{props.data.title}</h2>
-        <h3>{props.data.subtitle}</h3>
-        <p>
-        {props.data.author.firstName} {props.data.author.lastName}
-        </p>
-        <small>{showDate(props.data.date)}</small>
-    </div>
-    </header>
-)
-}
-
-// The app or parent or container component
+// The App, or the parent or the container component
 const App = () => {
-const data = {
-    welcome: 'Welcome to 30 Days Of React',
-    title: 'Getting Started React',
-    subtitle: 'JavaScript Library',
-    author: {
-    firstName: 'Mzee',
-    lastName: 'Msee',
-    },
-    date: new Date(), // date needs to be formatted to a human readable format
+const greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2024')
 }
 
 return (
     <div className='app'>
-    <Header data={data} />
+    <Button text='Greet People' onClick={greetPeople} />
+    <Button text='Show Time' onClick={() => alert(new Date())} />
     </div>
 )
 }
