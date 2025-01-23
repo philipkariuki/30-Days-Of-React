bca35a7272933c8c4e34de7696a9ef24a2eca102
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2025-01-23 23:21:44 +0300

    We update index.js where we show the first example of destructuring props through step by step destructuring

--- solutions/day-05/day-five-app/src/index.js
+++ solutions/day-05/day-five-app/src/index.js
@@ -1,14 +1,11 @@
+// i) Step by step destructuring:
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
-
-// e) Function props type:
-
-
-// Function to display time in Mon date, year format eg Oct 4, 2020
 const showDate = (time) => {
-const months = [
+  const months = [
     'January',
     'February',
     'March',
@@ -21,31 +18,51 @@ const months = [
     'October',
     'November',
     'December',
-]
+  ]
 
-const month = months[time.getMonth()].slice(0, 3)
-const year = time.getFullYear()
-const date = time.getDate()
-return ` ${month} ${date}, ${year}`
+  const month = months[time.getMonth()].slice(0, 3)
+  const year = time.getFullYear()
+  const date = time.getDate()
+  return ` ${month} ${date}, ${year}`
 }
-
-// Button component
-const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
+// Header Component
+const Header = (props) => {
+  const data = props.data
+  const { welcome, title, subtitle, author, date } = data
+  const { firstName, lastName } = author
+  return (
+    <header>
+      <div className='header-wrapper'>
+        <h1>{welcome}</h1>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>
+          {firstName} {lastName}
+        </p>
+        <small>{showDate(date)}</small>
+      </div>
+    </header>
+  )
+}
 
 // The app or parent or container component
 const App = () => {
-const handleTime = () => {
-    alert(showDate(new Date()))
-}
-const greetPeople = () => {
-    alert('Welcome to 30 Days Of React Challenge, 2024')
-}
-return (
+  const data = {
+    welcome: 'Welcome to 30 Days Of React',
+    title: 'Getting Started React',
+    subtitle: 'JavaScript Library',
+    author: {
+      firstName: 'Mzee',
+      lastName: 'Msee',
+    },
+    date: new Date(),
+  }
+
+  return (
     <div className='app'>
-    <Button text='show time' onClick={handleTime} />
-    <Button text='Greet People' onClick={greetPeople} />
+      <Header data={data} />
     </div>
-)
+  )
 }
 
 
