e1a1491ae5168a0d559541c45ebce72cf4947c5d
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2025-01-29 10:40:09 +0300

    Second example showing mapping an array of arrays, and we update index.js to show the result

--- solutions/day-06/day-six-app/src/index.js
+++ solutions/day-06/day-six-app/src/index.js
@@ -2,25 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const Numbers = ({ numbers }) => {
-// modifying array to array of li JSX
-const list = numbers.map((number) => <li>{number}</li>)
-return list
+const skills = [
+['HTML', 10],
+['CSS', 7],
+['JavaScript', 9],
+['React', 8],
+]
+
+// Skill Component
+const Skill = ({ skill: [tech, level] }) => (
+<li>
+    {tech} {level}
+</li>
+)
+
+// Skills Component
+const Skills = ({ skills }) => {
+const skillsList = skills.map((skill) => <Skill skill={skill} />)
+console.log(skillsList)
+return <ul>{skillsList}</ul>
 }
 
-// App component
 const App = () => {
-const numbers = [1, 2, 3, 4, 5]
-
 return (
-  <div className='container'>
-  <div>
-      <h1>Numbers List</h1>
-      <ul>
-      <Numbers numbers={numbers} />
-      </ul>
-  </div>
-  </div>
+    <div className='container'>
+    <div>
+        <h1>Skills Level</h1>
+        <Skills skills={skills} />
+    </div>
+    </div>
 )
 }
 
