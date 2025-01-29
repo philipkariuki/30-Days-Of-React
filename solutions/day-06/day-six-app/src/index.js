2cdd4e2b82b17641443b9b4b51983d7248c51c85
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2025-01-29 08:44:29 +0300

    The example given shows how an array of numbers are attached together in one line when the array is untouched ie is not yet modified to a JSX element
    We update index.js to show the result

--- solutions/day-06/day-six-app/src/index.js
+++ solutions/day-06/day-six-app/src/index.js
@@ -2,24 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <h1>Numbers List</h1>
+        {[1, 2, 3, 4, 5]}
+      </div>
     </div>
-  );
+  )
 }
 
 
