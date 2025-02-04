77469716169413f593cb078bec6838c0682b5127
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2025-02-04 17:23:13 +0300

    Solution for day 6 exercise level 3

--- solutions/day-06/day-six-app/src/index.js
+++ solutions/day-06/day-six-app/src/index.js
@@ -2,52 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+// Population data
+const tenHighestPopulation = [
+  { country: 'World', population: 7693165599 },
+  { country: 'China', population: 1377422166 },
+  { country: 'India', population: 1295210000 },
+  { country: 'USA', population: 323947000 },
+  { country: 'Indonesia', population: 258705000 },
+  { country: 'Brazil', population: 206135893 },
+  { country: 'Pakistan', population: 194125062 },
+  { country: 'Nigeria', population: 186988000 },
+  { country: 'Bangladesh', population: 161006790 },
+  { country: 'Russia', population: 146599183 },
+  { country: 'Japan', population: 126960000 },
+];
 
-
-// Function to generate a random hexadecimal color
-const getRandomHexColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-
-// Grid Component
-const Grid = () => {
-  const colors = Array.from({ length: 32 }, () => getRandomHexColor());
-
+const BarChart = () => {
   return (
-    <div className="grid-container">
-      {colors.map((color, index) => (
-        <div key={index} className="grid-item" style={{ backgroundColor: color }}>
-          {color}
-        </div>
-      ))}
+    <div className="container">
+      <h1>30 Days Of React</h1>
+      <h2 className="subheading">World Population</h2>
+      <p>Ten Most populated Countries</p>
+      <div className="chart-container">
+        {tenHighestPopulation.map((country, index) => (
+          <div key={index} className="chart-row">
+            <div className="country">{country.country}</div>
+            <div className="bar-container">
+              <div
+                className="bar"
+                style={{
+                  width: country.country === 'World' ? '100%' : `${(country.population / tenHighestPopulation[0].population) * 100}%`,
+                  height: 40,
+                }}
+              />
+            </div>
+            <div className="population">{country.population.toLocaleString()}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
-// Main App Component
-const App = ({ title, subtitle }) => {
-  return (
-    <div className="app">
-      <h1 className="heading">{title}</h1>
-      <h2 className="subheading">{subtitle}</h2>
-      <Grid />
-    </div>
-  );
-}
-
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App title="30 Days of React" subtitle="Hexadecimal Colors" />
+    <BarChart />
   </React.StrictMode>
 );
-
-
