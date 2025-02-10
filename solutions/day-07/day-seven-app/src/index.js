12ec70986c66ef4d7bab13a8ba1f7bda3fd05c03
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2025-02-10 16:13:43 +0300

    We show an exaple of a Class component and render it in index.js

--- solutions/day-07/day-seven-app/src/index.js
+++ solutions/day-07/day-seven-app/src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 
 // Header Component
-// Functional component
-const Header = () => (
-  <header>
-    <div className='header-wrapper'>
-      <h1>Welcome to 30 Days Of React</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
-      <p>Mzee Msee</p>
-      <small>Nov 29, 2024</small>
-    </div>
-  </header>
-)
+// Class based component
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className='header-wrapper'>
+          <h1>Welcome to 30 Days Of React</h1>
+          <h2>Getting Started React</h2>
+          <h3>JavaScript Library</h3>
+          <p>Mzee Msee</p>
+          <small>Nov 29, 2024</small>
+        </div>
+      </header>
+    )
+  }
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
